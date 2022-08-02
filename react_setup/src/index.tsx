db56256a6b7fb3ffcac57774160b92261f189c58
Miller <farwes1180@gmail.com>
2022-08-02 17:02:33 +0900

    function components

--- react_setup/src/index.tsx
+++ react_setup/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
 
-const App = () => {
-  return <div>Hello World</div>;
-};
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
-ReactDom.render(<App />, document.getElementById('root'));
+root.render(<App />);
