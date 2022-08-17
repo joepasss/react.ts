bc01bf223a868a290f288efa4ac5f839a7f93235
Miller <farwes1180@gmail.com>
2022-08-18 03:07:16 +0900

    Store cart data in local storage (useEffect hook)

--- react-typescript-setup/src/components/AppState.tsx
+++ react-typescript-setup/src/components/AppState.tsx
@@ -2,8 +2,8 @@ import React, {
   createContext,
   ReactNode,
   useContext,
+  useEffect,
   useReducer,
-  useState,
 } from 'react';
 
 interface CartItem {
@@ -41,7 +41,16 @@ interface AddToCartAction extends Action<'ADD_TO_CART'> {
   };
 }
 
-const reducer = (state: AppStateValue, action: AddToCartAction) => {
+interface InializeCartAction extends Action<'INITIALIZE_CART'> {
+  payload: {
+    cart: AppStateValue['cart'];
+  };
+}
+
+const reducer = (
+  state: AppStateValue,
+  action: AddToCartAction | InializeCartAction
+) => {
   if (action.type === 'ADD_TO_CART') {
     const itemToAdd = action.payload.item;
 
@@ -64,6 +73,8 @@ const reducer = (state: AppStateValue, action: AddToCartAction) => {
           : [...state.cart.items, { ...itemToAdd, quantity: 1 }],
       },
     };
+  } else if (action.type === 'INITIALIZE_CART') {
+    return { ...state, cart: action.payload.cart };
   }
 
   return state;
@@ -87,6 +98,21 @@ interface Props {
 
 const AppStateProvider: React.FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, defaultStateValue);
+
+  useEffect(() => {
+    const cart = window.localStorage.getItem('cart');
+
+    if (cart) {
+      dispatch({
+        type: 'INITIALIZE_CART',
+        payload: { cart: JSON.parse(cart) },
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem('cart', JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
     <AppStateContext.Provider value={state}>
