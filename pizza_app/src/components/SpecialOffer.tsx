5f74ddb72ef9eaacc8636a5013424a6ee3ba72a8
Miller <farwes1180@gmail.com>
2022-08-23 01:39:13 +0900

    Creating a Render Props component

--- pizza_app/src/components/SpecialOffer.tsx
+++ pizza_app/src/components/SpecialOffer.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import { PizzaDataType } from '../types';
-import { AddToCartProps, withAddToCart } from './AddToCart';
+import { WithAddToCartProps } from './AddToCart';
 import SpecialOfferCSS from './SpecialOffer.module.css';
 
-interface Props extends AddToCartProps {
+interface Props {
   pizza: PizzaDataType;
 }
 
-const SpecialOffer: React.FC<Props> = ({ pizza, addToCart }) => {
-  const handleAddToCartClick = () => {
-    addToCart({ id: pizza.id, name: pizza.name, price: pizza.price });
-  };
-
+const SpecialOffer: React.FC<Props> = ({ pizza }) => {
   return (
     <div className={SpecialOfferCSS.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
-      <button type='button' onClick={handleAddToCartClick}>
-        Add to Cart
-      </button>
+      <WithAddToCartProps>
+        {({ addToCart }) => {
+          return (
+            <button
+              type='button'
+              onClick={() =>
+                addToCart({
+                  id: pizza.id,
+                  name: pizza.name,
+                  price: pizza.price,
+                })
+              }
+            >
+              Add to Cart
+            </button>
+          );
+        }}
+      </WithAddToCartProps>
     </div>
   );
 };
 
-export default withAddToCart(SpecialOffer);
+export default SpecialOffer;
