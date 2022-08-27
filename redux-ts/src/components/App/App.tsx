42c56401ffa47416edaee77aa67ec98c8787b8ef
Miller <farwes1180@gmail.com>
2022-08-27 13:27:28 +0900

    Creating the Recorder Compontent

--- redux-ts/src/components/App/App.tsx
+++ redux-ts/src/components/App/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Recorder from "../Recorder";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <div className="App">
+      <Recorder />
+    </div>
+  );
 }
 
 export default App;
--- redux-ts/src/redux/store.ts
+++ redux-ts/src/redux/store.ts
@@ -1,17 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
+import { combineReducers, createStore } from "redux";
+import recorderReducer from "./recorder";
 import userEventsReducer from "./user-events";
 
 const rootReducer = combineReducers({
   userEvents: userEventsReducer,
+  recorder: recorderReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
 
 const store = configureStore({
-  reducer: {
-    rootReducer: rootReducer,
-  },
+  reducer: rootReducer,
 });
 
 export default store;
