5df1256bf7519c18c00051401fc03b916a30dcd8
Miller <farwes1180@gmail.com>
2022-09-01 16:37:58 +0900

    Loading Events

--- todo_app/src/redux/store.ts
+++ todo_app/src/redux/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import recorderReducer from "./recorder";
 import userEventsReducer from "./user-events";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   userEvents: userEventsReducer,
@@ -12,6 +13,7 @@ export type RootState = ReturnType<typeof rootReducer>;
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: [thunk],
 });
 
 export default store;
