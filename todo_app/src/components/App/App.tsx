7a1ac56d5b6c260b8c45101569380abb98ecff8d
Miller <farwes1180@gmail.com>
2022-09-01 16:04:06 +0900

    Creating the Event List Component

--- todo_app/src/components/App/App.tsx
+++ todo_app/src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Calendar from "../Calendar";
 import Recorder from "../Recorder";
 
 function App() {
   return (
     <div className="App">
       <Recorder />
+      <Calendar />
     </div>
   );
 }
