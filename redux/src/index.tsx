94110c1f3d000d8ec2d71fb8f8716b6952bf4e4f
Miller <farwes1180@gmail.com>
2022-09-06 10:53:32 +0900

    Link redux store to react app

--- redux/src/index.tsx
+++ redux/src/index.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 const root = ReactDom.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
