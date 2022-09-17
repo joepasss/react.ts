b8bad8eeb6555a45d3499eb4272f42ce80238f1b
Miller <farwes1180@gmail.com>
2022-09-18 06:03:34 +0900

    apollo server & graphql initialize

---
+++ task-mate/pages/api/graphql.ts
@@ -0,0 +1,51 @@
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { ApolloServer } from "apollo-server-micro";
+import { NextApiHandler } from "next";
+
+const typeDefs = /* GraphQL */ `
+  type Query {
+    users: [User!]!
+  }
+  type User {
+    name: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users() {
+      return [{ name: "Nextjs" }];
+    },
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [
+    ...(process.env.NODE_ENV === "development"
+      ? [ApolloServerPluginLandingPageGraphQLPlayground]
+      : []),
+  ],
+});
+
+let graphqlHandler: NextApiHandler | undefined;
+
+const handler: NextApiHandler = async (req, res) => {
+  if (!graphqlHandler) {
+    await apolloServer.start();
+    graphqlHandler = apolloServer.createHandler({
+      path: "/api/graphql",
+    });
+  }
+
+  return graphqlHandler(req, res);
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default handler;
