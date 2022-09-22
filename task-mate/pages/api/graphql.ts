e5d26e918a3c2096480c2be839769986deb6f44b
Miller <farwes1180@gmail.com>
2022-09-22 10:14:54 +0900

    Creating resolvers for the "tasks" query and "createTask" mutation

--- task-mate/pages/api/graphql.ts
+++ task-mate/pages/api/graphql.ts
@@ -1,13 +1,14 @@
 import { ApolloServer, gql } from "apollo-server-micro";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import mysql from "serverless-mysql";
+import { OkPacket } from "mysql";
 
 // Types
 import { NextApiHandler } from "next";
 import { IResolvers } from "@graphql-tools/utils";
 
 const typeDefs = gql`
-  enum TaskStaus {
+  enum TaskStatus {
     active
     completed
   }
@@ -15,7 +16,7 @@ const typeDefs = gql`
   type Task {
     id: Int!
     title: String
-    status: TaskStaus
+    status: TaskStatus
   }
 
   input CreateTaskInput {
@@ -25,11 +26,11 @@ const typeDefs = gql`
   input UpdateTaskInput {
     id: Int!
     title: String
-    status: TaskStaus
+    status: TaskStatus
   }
 
   type Query {
-    tasks(status: TaskStaus): [Task!]!
+    tasks(status: TaskStatus): [Task!]!
     task(id: Int!): Task
   }
 
@@ -44,24 +45,70 @@ interface ApolloContext {
   db: mysql.ServerlessMysql;
 }
 
+enum TaskStatus {
+  active = "active",
+  completed = "completed",
+}
+
+interface Task {
+  id: number;
+  title: string;
+  status: TaskStatus;
+}
+interface TaskDbRow {
+  id: number;
+  title: string;
+  task_status: TaskStatus;
+}
+
+type TasksDbQueryResult = TaskDbRow[];
+
 const resolvers: IResolvers<any, ApolloContext> = {
   Query: {
-    async tasks(parent, args, context) {
-      const result = await context.db.query(
-        'SELECT "HELLO WORLD" as hello_world'
+    async tasks(
+      parent,
+      args: { status?: TaskStatus },
+      context
+    ): Promise<Task[]> {
+      const { status } = args;
+      let query = "SELECT id, title, task_status FROM tasks";
+      const queryParams: string[] = [];
+
+      if (status) {
+        query += " WHERE task_status = ?";
+        queryParams.push(status);
+      }
+
+      const tasks = await context.db.query<TasksDbQueryResult>(
+        query,
+        queryParams
       );
       await db.end();
-      console.log({ result });
-
-      return [];
+      return tasks.map(({ id, title, task_status }) => ({
+        id,
+        title,
+        status: task_status,
+      }));
     },
     task(parent, args, context) {
       return null;
     },
   },
   Mutation: {
-    createTask(parent, args, context) {
-      return null;
+    async createTask(
+      parent,
+      args: { input: { title: string } },
+      context
+    ): Promise<Task> {
+      const result = await context.db.query<OkPacket>(
+        "INSERT INTO tasks (title, task_status) VALUES(?, ?)",
+        [args.input.title, TaskStatus.active]
+      );
+      return {
+        id: result.insertId,
+        title: args.input.title,
+        status: TaskStatus.active,
+      };
     },
     updateTask(parent, args, context) {
       return null;
