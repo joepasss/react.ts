e5d3ade278d5d1107439163c534c18a73e8b0fde
Miller <farwes1180@gmail.com>
2022-10-08 00:14:28 +0900

    Deleting Tasks

--- task-mate/components/TaskListItem.tsx
+++ task-mate/components/TaskListItem.tsx
@@ -1,12 +1,48 @@
+import { Reference } from "@apollo/client";
 import Link from "next/link";
-import React, { FC } from "react";
-import { Task } from "../generated/graphql-frontend";
+import React, { FC, useEffect } from "react";
+import { Task, useDeleteTaskMutation } from "../generated/graphql-frontend";
 
 interface Props {
   task: Task;
 }
 
 const TaskListItem: FC<Props> = ({ task }) => {
+  const [deleteTask, { loading, error }] = useDeleteTaskMutation({
+    variables: { id: task.id },
+    errorPolicy: "all",
+
+    update: (cache, result) => {
+      const deletedTask = result.data?.deleteTask;
+
+      if (deletedTask) {
+        cache.modify({
+          fields: {
+            tasks(taskRefs: Reference[], { readField }) {
+              return taskRefs.filter((taskRef) => {
+                return readField("id", taskRef) !== deletedTask.id;
+              });
+            },
+          },
+        });
+      }
+    },
+  });
+
+  const handleDeleteClick = async () => {
+    try {
+      await deleteTask();
+    } catch (error) {
+      // Log the error
+    }
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert("An error occured, please try again");
+    }
+  }, [error]);
+
   return (
     <li className="task-list-item">
       <Link href={"/update/[id]"} as={`/update/${task.id}`}>
@@ -15,7 +51,13 @@ const TaskListItem: FC<Props> = ({ task }) => {
         </a>
       </Link>
 
-      <button className="task-list-item-delete">&times;</button>
+      <button
+        className="task-list-item-delete"
+        disabled={loading}
+        onClick={handleDeleteClick}
+      >
+        &times;
+      </button>
     </li>
   );
 };
